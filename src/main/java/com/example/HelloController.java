e09db7df3a9089424353bbc72c8b0b00658d0283
nakamura <nakamura>
2016-03-22 19:34:52 +0900

    繰り返し出力

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,7 +13,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String hello() {
-		return "hello";
+	public String hello(Model model) {
+		List<Hoge> list = Arrays.asList(new Hoge() {
+			{
+				id = 10;
+				value = "hoge";
+			}
+		}, new Hoge() {
+			{
+				id = 20;
+				value = "fuga";
+			}
+		}, new Hoge() {
+			{
+				id = 30;
+				value = "piyo";
+			}
+		});
+
+		model.addAttribute("hogeList", list);
+
+		return "hello hotdelpoy";
 	}
+
 }
---
+++ src/main/java/com/example/Hoge.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Hoge {
+
+	public int id;
+
+	public String value;
+
+	@Override
+	public String toString() {
+		return "Hoge [id=" + id + ", value=" + value + "]";
+	}
+
+}
